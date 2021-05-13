0e782186f1e7037b617bd34c2a6956824afa695a
Rajat Arora <rajatarora@Rajats-MacBook-Pro.local>
2021-05-13 18:19:11 +0530

    Implemented useState React Hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import  ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -5,15 +6,17 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title)
+
     const clickHandler = () => {
-        console.log("Clicked!!!");
+        setTitle('Updated!!!')
     }
 
     return (
         <Card className="expense-item">
              <ExpenseDate  date = {props.date}></ExpenseDate>
               <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
                 <button onClick = {clickHandler}>Click</button>
             </div>
