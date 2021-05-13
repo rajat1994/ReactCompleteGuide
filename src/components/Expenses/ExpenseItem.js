3ad0c86399751885c592bf6267730788bad6bb5b
Rajat Arora <rajatarora@Rajats-MacBook-Pro.local>
2021-05-13 17:52:20 +0530

    Added onclick event handler props

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,17 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+    const clickHandler = () => {
+        console.log("Clicked!!!");
+    }
+
     return (
         <Card className="expense-item">
              <ExpenseDate  date = {props.date}></ExpenseDate>
               <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
+                <button onClick = {clickHandler}>Click</button>
             </div>
         </Card>
     );
